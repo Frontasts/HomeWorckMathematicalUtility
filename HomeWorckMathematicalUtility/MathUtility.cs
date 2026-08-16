e926a87cddabd9c1e28aa0bddbaa28df03c2f553
Frontasts <steamfronts0110@gmail.com>
2026-08-16 22:41:31 +0300

    Добавленны статик методы деления и умножения требующиеся по условиям задачи

--- HomeWorckMathematicalUtility/MathUtility.cs
+++ HomeWorckMathematicalUtility/MathUtility.cs
@@ -17,5 +17,20 @@ namespace HomeWorckMathematicalUtility
         {
             return a - b;
         }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                return 0;
+            }
+
+            return a / b;
+        }
     }
 }
