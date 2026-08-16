f0e565fbec9fd586a98e5686b9da14166c85bc73
Frontasts <steamfronts0110@gmail.com>
2026-08-16 22:39:41 +0300

    Добавленн статические методы сложения и вычитания требующиеся по условиям задачи

--- HomeWorckMathematicalUtility/MathUtility.cs
+++ HomeWorckMathematicalUtility/MathUtility.cs
@@ -8,5 +8,14 @@ namespace HomeWorckMathematicalUtility
 {
     public static class MathUtility
     {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
     }
 }
