af75706786f20693b3f2e621fd3733d0990714eb
Frontasts <steamfronts0110@gmail.com>
2026-08-16 23:02:22 +0300

    Добавлена демонстрация работы статик класса

--- HomeWorckMathematicalUtility/Program.cs
+++ HomeWorckMathematicalUtility/Program.cs
@@ -10,6 +10,29 @@ namespace HomeWorckMathematicalUtility
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("=== Математическая Утилита ===");
+            Console.WriteLine();
+
+            Random random = new Random();
+            double x = random.Next(0, 1000);
+            double y = random.Next(0, 1000);
+
+            double sum = MathUtility.Add(x, y);
+            Console.WriteLine($"{x} + {y} = {sum}");
+
+            double diff = MathUtility.Subtract(x, y);
+            Console.WriteLine($"{x} - {y} = {diff}");
+
+            double prod = MathUtility.Multiply(x, y);
+            Console.WriteLine($"{x} * {y} = {prod}");
+
+            double quot = MathUtility.Divide(x, y);
+            Console.WriteLine($"{x} / {y} = {quot}");
+
+            Console.WriteLine();
+
+            double bad = MathUtility.Divide(x, 0);
+            Console.WriteLine($"Результат деления на 0: {bad}");
         }
     }
 }
